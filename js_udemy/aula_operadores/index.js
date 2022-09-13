2bd246d58f1d244507515cc375d46e9799d67e2c
amaur1mmj <amaur1mmj@gmail.com>
2022-09-13 17:15:40 -0300

    okok

---
+++ js_udemy/aula_operadores/index.js
@@ -0,0 +1,31 @@
+/*
+! Operadores de comapração 
+
+> mmaior que
+>= maior igual
+< menor que
+<= menor igual 
+
+== igual (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente restrito
+
+*/
+
+const n = 10
+const m = '10'
+
+const comp = n === m
+//console.log(comp)
+
+/*
+! Operadores Lógicos
+
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+
+*/
+
+console.log((0 || true || 'carlim') && 0)
