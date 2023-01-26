44c1d48efe5bec08c1fca0b700732bcc5280f474
amaur1mmj <amaur1mmj@gmail.com>
2023-01-26 10:21:42 -0300

    Calculadora de IMC finalizada.
    Aprendizado sobre:
    	-separar as funções de forma devida para que cada uma seja única
    	-ciração de class e tags usando JS
    	-algo legal sobre prioridade e validação de ações

--- js_udemy/exercicio_aula44/assets/js/main.js
+++ js_udemy/exercicio_aula44/assets/js/main.js
@@ -1,18 +1,66 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento prevenido');
 
-    setResultado('HElooooooooooooo!');
+    const inputPeso = e.target.querySelector('#input-peso');
+    const inputAltura = e.target.querySelector('#input-altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        return setResultado('Peso inválido', false);
+    }
+    if (!altura) {
+        return setResultado('Altura inválida', false);
+    }
+    const imc = getIMC(peso, altura);
+    const nivelIMC = yourIMC(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelIMC})`;
+
+    setResultado(msg, true);
+
 });
 
-function setResultado(msg){
-    const result = document.querySelector('#result');
-    result.innerHTML = '';
+function getIMC(peso, altura) {
+    const imc = peso / (altura ** 2);
+    return imc.toFixed(2);
+}
+
+function yourIMC(imc) {
+    const nivel = ['Obesidade grau 3!', 'Obesidade grau 2!', 'Obesidade grau 1!', 'Sobrepeso!', 'Peso normal!', 'Abaixo do peso!']
+
+    if (imc >= 39.9) return nivel[0];
+    if (imc >= 34.9) return nivel[1];
+    if (imc >= 29.9) return nivel[2];
+    if (imc >= 24.9) return nivel[3];
+    if (imc >= 18.5) return nivel[4];
+    if (imc < 18.5) return nivel[5];
+
+}
+
+
+
+function getP() {
     const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado');
+    return p;
+
+}
+
+function setResultado(msg, isValid) {
+    const result = document.querySelector('#result');
+    result.innerHTML = ''
+
+    const p = getP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
     p.innerHTML = msg;
-    result.append(p);
+    result.appendChild(p);
 
 }
