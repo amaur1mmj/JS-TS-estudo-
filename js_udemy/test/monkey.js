2264ade47409166ab2ca940d3b68d272610e24cf
amaur1mmj <amaur1mmj@gmail.com>
2023-05-29 12:13:43 -0300

    aulas usando webpack e conceitos de estruturas de pastas e import

---
+++ js_udemy/test/monkey.js
@@ -0,0 +1,15 @@
+const day = 'friday';
+const n = 10;
+//vai de 0 a 10
+const funkiness = 7;
+
+let mkParty = 'monkey'
+
+if(day === 'friday') mkParty = 'frinday happy!!';
+
+if(funkiness >= 7) mkParty = mkParty.toUpperCase()
+
+for(let i = 0; i <= n; i++){
+    console.log(mkParty);
+}
+
